a1d9f9e3a6bd3161d57468e97e2161b918765907
KevinTechen <kevin.techen@xivotec.com>
2025-05-19 14:56:04 +0200

    add error handling

---
+++ src/main/java/org/example/required4testing/controllers/CustomErrorController.java
@@ -0,0 +1,13 @@
+package org.example.required4testing.controllers;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+    @RequestMapping("/error")
+    public String handleError() {
+        return "redirect:/error.xhtml";
+    }
+}
